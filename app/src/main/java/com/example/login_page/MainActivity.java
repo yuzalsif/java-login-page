d4ebfb1bbd3a1d7e6460192629e20f2c015b1ab9
yuzalsif <liteit68@gmail.com>
2023-08-07 16:58:35 +0300

    feat: Add showing the message upon login

--- app/src/main/java/com/example/login_page/MainActivity.java
+++ app/src/main/java/com/example/login_page/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.login_page;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText password = null;
 
     MaterialButton loginBtn = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
 
         loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userName.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
