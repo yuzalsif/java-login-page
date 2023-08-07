0ab927153bb65b668624d480445ca1197d1e65f6
yuzalsif <liteit68@gmail.com>
2023-08-07 16:49:43 +0300

    Add buttons and edit text references

--- app/src/main/java/com/example/login_page/MainActivity.java
+++ app/src/main/java/com/example/login_page/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.login_page;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText userName = null;
+    EditText password = null;
+
+    MaterialButton loginBtn = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = (EditText) findViewById(R.id.userName);
+        password = (EditText) findViewById(R.id.password);
+
+        loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
     }
 }
