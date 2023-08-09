924745d7b407586f0a43474bf79b1c38fbccc463
yuzalsif <liteit68@gmail.com>
2023-08-09 11:22:26 +0300

    Add model to store jokes

---
+++ app/src/main/java/com/example/login_page/JokeModel.java
@@ -0,0 +1,17 @@
+package com.example.login_page;
+
+public class JokeModel {
+    private String joke;
+
+    public JokeModel(String joke) {
+        this.joke = joke;
+    }
+
+    public String getJoke() {
+        return this.joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
